f9791808dec6c081fb5afefb5d632466216993a5
Artify <richard.tichy@mensa.cz>
2021-12-25 21:52:50 +0100

    :art: Added external setting of host address and port.

--- web_api/src/main.rs
+++ web_api/src/main.rs
@@ -1,17 +1,40 @@
 use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+use std::env;
 
-#[get("/hello")]
+#[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("world!")
+    HttpResponse::Ok().body("Secret Hitler's API")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    println!("Len of args: {}.", args.len());
+    println!("Arg[0] is: {}.", &args[0]);
+
+    let mut address: String = "0.0.0.0".to_string();
+    let mut port: u16 = 8000;
+
+    //  1st argument is the binary command
+    if (args.len() - 1) == 2 {
+        address = (&args[1]).into_string();
+        port = (&args[2]).parse().unwrap();
+    } else if (args.len() - 1) == 1 {
+        port = (&args[1]).parse().unwrap();
+    }
+
+    let address = address;
+    let port = port;
+
+    println!("Running on port {} on host {}.", port, address);
+    println!("http://{}:{}/", address, port);
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
     })
-        .bind("127.0.0.1:8000")?
+        .bind(format!("{}:{}", address, port))?
         .run()
         .await
 }
