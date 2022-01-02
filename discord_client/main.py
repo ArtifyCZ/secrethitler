fd0935132fda9a4238dc6f827e183827ac6547f7
Poirot12345 T <fanda987654@seznam.cz>
2022-01-02 16:14:06 +0100

    on_ready welcome message to terminal

--- discord_client/main.py
+++ discord_client/main.py
@@ -8,7 +8,13 @@ guild_ids = [int(config("GUILD"))]
 
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
+    print(f"""
+    Welcome to Secter Hitler Discord bot!
+    This Discord bot is supposed to bridge Secret Hitler game to Discord,
+    so you can play along with your friends there.
+
+    Logged as {bot.user}
+    """)
 
 @bot.slash_command(guild_ids=guild_ids)
 async def ping(ctx):
