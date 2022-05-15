0fffaa5121925fea51630cd1fab6841e290a8b1e
Artify <richard.tichy@mensa.cz>
2022-05-15 14:49:41 +0200

    Github workflow - 6th fix try.

--- web_api/tests/index.js
+++ web_api/tests/index.js
@@ -9,25 +9,10 @@ const request = supertest(`http://${HOST}:${PORT}`)
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-const { exec } = require('child_process')
-let server = null
-const before = async () => {
-    server = exec('cargo run --release')
-    await delay(1000)
-}
-
-const after = async () => {
-    server.kill()
-    await delay(300)
-}
-
 const checkUUIDValidity = (uuid) => {
     const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
     return regexExp.test(uuid)
 }
-
-beforeEach(before, 100000)
-afterEach(after, 1000)
 
 module.exports = {
     request,
