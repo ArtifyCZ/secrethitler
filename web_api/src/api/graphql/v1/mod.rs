3c80a49064c140ddebb48c82b154b75d61d1f070
Artify <richard.tichy@mensa.cz>
2022-06-06 17:26:16 +0200

    @D4VID0x2 requested code.

--- web_api/src/api/graphql/v1/mod.rs
+++ web_api/src/api/graphql/v1/mod.rs
@@ -1,15 +1,19 @@
-use std::time::Duration;
-use juniper::RootNode;
+use std::{time::Duration, convert::Infallible};
+use std::ptr::null;
+use juniper::{RootNode, ScalarValue};
 use actix_web::{Error, HttpRequest, HttpResponse, Scope, web::{ Data, get, Payload, post }};
-use juniper_graphql_ws::ConnectionConfig;
+use futures::StreamExt;
+use juniper_graphql_ws::{ConnectionConfig, Init};
 use juniper_actix::{graphql_handler, subscriptions::subscriptions_handler};
+use uuid::Uuid;
 use crate::api::graphql::v1::context::GraphQLContext;
 use crate::api::graphql::v1::mutation::Mutation;
 use crate::api::graphql::v1::subscription::Subscription;
 use crate::api::graphql::v1::query::Query;
 //use crate::api::graphql::v1::websocket::websocket_gql_endpoint;
 use crate::app::slot::slot_service::SlotService;
-use crate::app::user::user::User;
+use crate::app::user::user::{User, UserType};
+use crate::AuthService;
 
 mod query;
 mod object;
@@ -39,19 +43,31 @@ async fn graphql_api_endpoint(
     graphql_handler(schema.get_ref(), &context, req, payload).await
 }
 
+pub struct SubscriptionConfig<CtxT: Unpin + Send + 'static>(ConnectionConfig<CtxT>);
+impl<S: ScalarValue, CtxT: Unpin + Send + 'static> Init<S, CtxT> for SubscriptionConfig<CtxT> {
+    type Error = Infallible;
+    type Future = futures::future::Ready<Result<ConnectionConfig<CtxT>, Self::Error>>;
+
+    fn init(self, params: juniper::Variables<S>) -> Self::Future {
+        println!("It fuckin' did it!");
+        futures::future::ok(self.0)
+    }
+}
+
 async fn socket_gql_endpoint(
     req: HttpRequest,
-    payload: Payload,
+    mut payload: Payload,
     schema: Data<Schema>,
     slots: Data<SlotService>,
-    user: User
+    // auth: Data<AuthService>
+    // user: User
     ) -> Result<HttpResponse, Error> {
-    println!("Hello world!");
-    let context = GraphQLContext::new(slots.get_ref().clone(), user);
+    // TODO: This should really be replaced.
+    let context = GraphQLContext::new(slots.get_ref().clone(), User::new(Uuid::new_v4(), UserType::Temp, "abc".to_string()));
     let config = ConnectionConfig::new(context);
     let config = config.with_keep_alive_interval(Duration::from_secs(15));
 
-    subscriptions_handler(req, payload, schema.into_inner(), config).await
+    subscriptions_handler(req, payload, schema.into_inner(), SubscriptionConfig(config)).await
 }
 
 async fn graphiql_route() -> Result<actix_web::HttpResponse, actix_web::Error> {
