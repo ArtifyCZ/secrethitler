e78d178419fc3b0265af7e9701f94283aa601df9
Richard Tichý <tichy@artify.zone>
2023-03-13 13:47:45 +0100

    Implement connection pool init on start

--- web_api/src/main.rs
+++ web_api/src/main.rs
@@ -1,10 +1,27 @@
 #![deny(unsafe_code)]
 
+use std::time::Duration;
 use anyhow::Result;
 use axum::Router;
+use sea_orm::{Database, DatabaseConnection};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let db: DatabaseConnection = {
+        let mut opt = sea_orm::ConnectOptions::new(
+            std::env::var("DB_URL")
+                .unwrap_or_else(|_| "postgresql://localhost/secrethitler".to_string()));
+        opt.max_connections(100)
+            .min_connections(5)
+            .connect_timeout(Duration::from_secs(8))
+            .acquire_timeout(Duration::from_secs(8))
+            .idle_timeout(Duration::from_secs(8))
+            .max_lifetime(Duration::from_secs(8))
+            .sqlx_logging(true)
+            .set_schema_search_path("my_schema".into());
+        Database::connect(opt).await?
+    };
+
     let app = Router::new()
         .route("/", axum::routing::get(|| async { "Hello world!" }));
 
