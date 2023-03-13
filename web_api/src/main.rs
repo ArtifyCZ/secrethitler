eaafd58bf5449cf07a65ce68155c5b543b2ffd9a
Richard Tichý <tichy@artify.zone>
2023-03-13 12:30:24 +0100

    Init basic web server

--- web_api/src/main.rs
+++ web_api/src/main.rs
@@ -1,4 +1,19 @@
+use anyhow::Result;
+use axum::Router;
+
 #[tokio::main]
-async fn main() {
-    println!("Hello world!");
+async fn main() -> Result<()> {
+    let app = Router::new()
+        .route("/", axum::routing::get(|| async { "Hello world!" }));
+
+    let address = match std::env::var("HOST") {
+        Ok(str) => str,
+        Err(_) => "0.0.0.0:3000".to_string(),
+    }.parse()?;
+
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
 }
