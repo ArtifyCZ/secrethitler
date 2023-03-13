6ea11e2ea2640dfc7d678abcfc7a324fa0099348
Richard Tichý <tichy@artify.zone>
2023-03-13 17:29:31 +0100

    Implement anonymous account creation

--- web_api/app/src/auth.rs
+++ web_api/app/src/auth.rs
@@ -1,8 +1,60 @@
-use sea_orm::DatabaseConnection;
+use anyhow::bail;
+use uuid::Uuid;
+use sea_orm::*;
+use ::entity::{account, auth_token};
+
+pub struct CreateAnonymousAccountInputDto {
+    pub username: String,
+}
+
+pub struct CreateAnonymousAccountOutputDto {
+    pub token_id: Uuid,
+    pub token: Uuid,
+}
 
 pub struct AuthService<'a> {
     database: &'a DatabaseConnection,
 }
 
 impl<'a> AuthService<'a> {
+    pub async fn create_anonymous_account(&self, input: CreateAnonymousAccountInputDto)
+                                              -> anyhow::Result<CreateAnonymousAccountOutputDto> {
+        let CreateAnonymousAccountInputDto { username } = input;
+
+        if account::Entity::find()
+            .filter(account::Column::Username.eq(&username))
+            .limit(1)
+            .count(self.database).await? > 0 {
+            bail!("Username `{}` already in use.", username);
+        }
+
+        let id = Uuid::new_v4();
+
+        let account = account::ActiveModel {
+            id: Set(id),
+            username: Set(username),
+        };
+
+        let token_id = Uuid::new_v4();
+        let token = Uuid::new_v4();
+
+        let auth_token = auth_token::ActiveModel {
+            id: Set(token_id),
+            token: Set(token),
+            account_id: Set(id),
+        };
+
+        self.database.transaction::<_, (), DbErr>(|txn| {
+            Box::pin(async move {
+                account.save(txn).await?;
+                auth_token.save(txn).await?;
+                Ok(())
+            })
+        }).await?;
+
+        Ok(CreateAnonymousAccountOutputDto {
+            token_id,
+            token,
+        })
+    }
 }
