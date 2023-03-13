65bf7a9ae35e6648be5fb7f0d4fe3de458657246
Richard Tichý <tichy@artify.zone>
2023-03-13 18:08:43 +0100

    Define anonymous account creation error type

--- web_api/app_contract/src/auth.rs
+++ web_api/app_contract/src/auth.rs
@@ -1,4 +1,6 @@
 use async_trait::async_trait;
+use sea_orm::{DbErr, TransactionError};
+use thiserror::Error;
 use uuid::Uuid;
 
 pub struct CreateAnonymousAccountInputDto {
@@ -8,6 +10,23 @@ pub struct CreateAnonymousAccountInputDto {
 pub struct CreateAnonymousAccountOutputDto {
     pub token_id: Uuid,
     pub token: Uuid,
+}
+
+#[derive(Error, Debug)]
+pub enum CreateAnonymousAccountError {
+    #[error("Username `{0}` is already in use")]
+    UsernameAlreadyInUse(String),
+    #[error("An database error occurred: {0}")]
+    DatabaseError(#[from] DbErr),
+}
+
+impl From<TransactionError<DbErr>> for CreateAnonymousAccountError {
+    fn from(value: TransactionError<DbErr>) -> Self {
+        match value {
+            TransactionError::Connection(err) => err,
+            TransactionError::Transaction(err) => err,
+        }.into()
+    }
 }
 
 #[async_trait]
