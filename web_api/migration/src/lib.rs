6fa2d1b1b853acaf0a487dce387339fc60466652
Richard Tichý <tichy@artify.zone>
2023-03-14 13:34:34 +0100

    Fix not migrating `auth_token` into database

--- web_api/migration/src/lib.rs
+++ web_api/migration/src/lib.rs
@@ -10,6 +10,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20230313_131107_create_account_table::Migration),
+            Box::new(m20230313_152634_create_auth_token_table::Migration),
         ]
     }
 }
